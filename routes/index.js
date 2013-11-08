72eab97ba47ea32fde6d3994980a5013e87f620c
zz chen <grantchen88@gmail.com>
2013-11-08 10:03:14 +0800

    2

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,32 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 };
 
 exports.user = function(req,res){
-	res.send("user");
+	res.render('user', { title: 'Express' });
 };
 
 exports.post = function(req,res){
-	res.send("post");
+	res.render('user', { title: 'Express' });
 };
 
 exports.reg = function(req,res){
-	res.send("register");
+	res.render('reg', { title: 'Express' });
 };
 
 exports.doReg = function(req,res){
-	res.send("do register");
+	console.log(req);
+	res.send("do reg");
 };
 
 exports.login = function(req,res){
-	res.send("login");
+	res.render('login', { title: 'Express' });
 };
 
 exports.doLogin = function(req,res){
+	console.log(req);
 	res.send("do login");
 };
 
